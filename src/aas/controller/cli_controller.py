7431cbe70df73c8c068669a29d21c8471d75fe00
DukiMy <durim.miziraj@gmail.com>
2026-08-22 12:52:46 +0200

    Prepared for componentization

--- src/aas/controller/cli_controller.py
+++ src/aas/controller/cli_controller.py
@@ -1,9 +1,8 @@
 # Copyright (c) 2026 Durim Miziraj.
 # --------------------------------------------------------------------
 
-
-# TODO: Remove all prints to view.
-#   - Create a new info signal for printing to the view, signals must pass the domain.
+# TODO: Create separate components for each command. And let the com-
+#       ponents subscribe the the model with their specific event.
 
 """Provide the CLI controller."""
 
@@ -75,18 +74,337 @@ class CLIController:
 
         return first, remainder
 
-    def _take_command(self) -> str:
-        """Read a command from the user.
+    def _parse_load_image(
+        self, command: str
+    ) -> LoadImageRequested | None:
+        """Parse 'load image' command.
+
+        Param command:
+            A command for loading images.
 
         Returns:
-            A string of commands with no spaces at [0] and [-1].
+            An event representing a request to load an image.
+        
+        """
+        parts = command.split()
+
+        if len(parts) == 1:
+            filename = parts[0]
+
+            return LoadImageRequested(
+                filename,
+                None,
+            )
+
+        if (len(parts) == 3 and parts[1].lower() == "as"):
+            filename = parts[0]
+            alias = parts[2]
+
+            return LoadImageRequested(filename, alias)
+
+        return DisplayWarningRequested(
+            f"Invalid command: {command}"
+        )
+
+    def _parse_load_session(
+        self, command: str
+    ) -> LoadSessionRequested | None:
+        """Parse 'load session' command.
+        
+        Param command:
+            A command for loading a session.
+
+        Returns:
+            An event representing a request for loading a session.
 
         """
-        self._notify_observers(
-            DisplayNoticeRequested("AAS: ")
-        )
-        return input().strip()
+        parts = command.split()
 
+        if len(parts) != 1:
+            return DisplayWarningRequested(
+                f"Invalid command: {command}"
+            )
+
+        filename = parts[0]
+
+        return LoadSessionRequested(filename)
+
+    def _parse_width(
+        self, image: str, value: str
+    ) -> ChangeWidth | None:
+        """Parse a width value.
+
+        Param image:
+            The reference to the image whos width will be changed.
+        
+        Param value:
+            The value of the new width.
+
+        Returns:
+            An event representing a request for changing the width of
+            of the image under the provided reference.
+
+        """
+        try:
+            width = int(value)
+
+        except ValueError:
+            return DisplayWarningRequested(
+                f"Invalid width value: {value}"
+            )
+
+        if width <= 0:
+            return DisplayWarningRequested(
+                f"Invalid width: {width}"
+            )
+
+        return ChangeWidth(image, width)
+
+    def _parse_height(
+        self, image: str, value: str
+    ) -> ChangeHeight | None:
+        """Parse a height value.
+
+        Param image:
+            A reference to the image whos height will be changed.
+
+        Param value:
+            The value of the new height.
+
+        Returns:
+            An event representing a request for changing the height of
+            the image under the provided reference.
+
+        """
+        try:
+            height = int(value)
+
+        except ValueError:
+            return DisplayWarningRequested(
+                f"Invalid height value: {value}"
+            )
+
+        if height <= 0:
+            return DisplayWarningRequested(
+                f"Invalid height: {height}"
+            )
+
+        return ChangeHeight(image, height)
+
+    def _parse_brightness(
+        self, image: str, value: str
+    ) -> ChangeBrightness | None:
+        """Parse a brightness value.
+
+        Param image:
+            A reference to the image who brightness will change.
+
+        Param value:
+            The value of the new brightness.
+
+        Returns:
+            An event representing a request for changing the brightne-
+            ss of the image under the provided reference.
+
+        """
+        try:
+            brightness = float(value)
+
+        except ValueError:
+            return DisplayWarningRequested(
+                f"Invalid brightness value: {value}"
+            )
+
+        if brightness < 0:
+            return DisplayWarningRequested(
+                f"Invalid brightness: {brightness}"
+            )
+
+        return ChangeBrightness(image, brightness)
+
+    def _parse_contrast(
+        self, image: str, value: str
+    ) -> ChangeContrast | None:
+        """Parse a contrast value.
+
+        Param image:
+            A reference to the image whos contrast will change.
+
+        Param value:
+            The value of the new contrast.
+
+        Returns:
+            An event representing a request for changing the contrast
+            of the image under the provided reference.
+
+        """
+        try:
+            contrast = float(value)
+        except ValueError:
+            return DisplayWarningRequested(
+                f"Invalid contrast value: {value}"
+            )
+
+        if contrast < 0:
+            return DisplayWarningRequested(
+                f"Invalid contrast: {contrast}"
+            )
+
+        return ChangeContrast(image, contrast)
+
+    def _parse_quit_command(
+        self, command: str
+    ) -> ExitRequested:
+        """Parse a quit command.
+
+        Param command:
+            An exit command which will be parsed.           
+        Returns:
+            A controllerevent representing a request to exit the appl-
+            ication.
+            
+        """
+        return ExitRequested()
+
+    def _parse_info_command(
+        self, command: str
+    ) -> ImageInfoRequested:
+        """Parse an info command.
+
+        Param command:
+            An info request command that will be parsed.
+
+        Returns:
+            A controllerevent representing a request to display infor-
+            mation of an image.
+
+        """
+        return ImageInfoRequested()
+
+
+    def _parse_save_command(
+        self, command: str
+    ) -> SaveSessionRequested | None:
+        """Parse a save command.
+
+        Param command:
+            A 'save session as' command which will be parsed.
+
+        Returns:
+            A controller event representing a request to save a
+            session, or a none if the command is malformed.
+
+        """
+
+        parts = command.split()
+
+        if (len(parts) == 3
+            and parts[0].lower() == "session"
+            and parts[1].lower() == "as"
+        ):
+            filename = parts[2]
+
+            return SaveSessionRequested(filename)
+
+        return DisplayWarningRequested(
+            f"Invalid save command: {command}"
+        )
+
+    def _parse_render_command(
+        self, command: str
+    ) -> RenderRequested | None:
+        """Parse a render command.
+
+        Param command:
+            A 'render' command which will be parsed.
+
+        Returns:
+            A controller event representing a request to render an im-
+            age, or 'None' if it fails to parse the 'set' command.
+
+        """
+        parts = command.split()
+
+        if not parts:
+            return RenderRequested()
+
+        if len(parts) == 1:
+            return RenderRequested(
+                image=parts[0]
+            )
+
+        if (
+            len(parts) == 3
+            and parts[1].lower() == "to"
+        ):
+            return RenderRequested(
+                image=parts[0], destination=parts[2]
+            )
+
+        return DisplayWarningRequested(
+            f"Invalid render command: {command}"
+        )
+
+    def _parse_set_command(
+        self, command: str
+    ) -> ControllerEvent | None:
+        """Parse a set command.
+
+        Param command:
+            The 'set' command which will be parsed.
+
+        Returns:
+            A controller event related to the 'set' command, or a
+            'None' if any of the methods delegates fail.
+
+        """
+        parts = command.split()
+
+        if len(parts) != 3:
+            return DisplayWarningRequested(
+                f"Invalid command: {command}"
+            )
+
+        image = parts[0]
+        property_name = parts[1].lower()
+        value = parts[2]
+
+        if property_name == "width":
+            return self._parse_width(image, value)
+
+        if property_name == "height":
+            return self._parse_height(image, value)
+
+        if property_name == "brightness":
+            return self._parse_brightness(image, value)
+
+        if property_name == "contrast":
+            return self._parse_contrast(image, value)
+
+        return DisplayWarningRequested(f"Invalid command: {command}")
+
+    def _parse_load_command(
+        self, command: str
+    ) -> ControllerEvent | None:
+        """Parse a load command.
+
+        Param command:
+            A command to load either an image or a session.
+
+        Returns:
+            An event reprsenting a request for loading a session or an
+            image.
+
+        """
+        subcommand, remainder = self._split_first(command)
+
+        if subcommand == "image":
+            return self._parse_load_image(remainder)
+
+        if subcommand == "session":
+            return self._parse_load_session(remainder)
+
+        return DisplayWarningRequested(f"Invalid command: {command}")
 
     def _parse_command(self, command: str) -> ControllerEvent | None:
         """Parse a complete CLI command.
@@ -124,311 +442,17 @@ class CLIController:
 
         return DisplayWarningRequested(f"Invalid command: {command}")
 
-    def _parse_load_image(
-        self, command: str
-    ) -> LoadImageRequested | None:
-        """Parse 'load image' command.
-
-        Param command:
-            A command for loading images.
+    def _take_command(self) -> str:
+        """Read a command from the user.
 
         Returns:
-            An event representing a request to load an image.
-        
-        """
-        parts = command.split()
-
-        if len(parts) == 1:
-            filename = parts[0]
-
-            return LoadImageRequested(
-                filename,
-                None,
-            )
-
-        if (len(parts) == 3 and parts[1].lower() == "as"):
-            filename = parts[0]
-            alias = parts[2]
-
-            return LoadImageRequested(filename, alias)
-
-        return DisplayWarningRequested(f"Invalid command: {command}")
-
-    def _parse_load_session(
-        self, command: str
-    ) -> LoadSessionRequested | None:
-        """Parse 'load session' command.
-        
-        Param command:
-            A command for loading a session.
-
-        Returns:
-            An event representing a request for loading a session.
+            A string of commands with no spaces at [0] and [-1].
 
         """
-        parts = command.split()
-
-        if len(parts) != 1:
-            return DisplayWarningRequested(f"Invalid command: {command}")
-
-        filename = parts[0]
-
-        return LoadSessionRequested(filename)
-
-    def _parse_load_command(
-        self, command: str
-    ) -> ControllerEvent | None:
-        """Parse a load command.
-
-        Param command:
-            A command to load either an image or a session.
-
-        Returns:
-            An event reprsenting a request for loading a session or an
-            image.
-
-        """
-        subcommand, remainder = self._split_first(command)
-
-        if subcommand == "image":
-            return self._parse_load_image(remainder)
-
-        if subcommand == "session":
-            return self._parse_load_session(remainder)
-
-        return DisplayWarningRequested(f"Invalid command: {command}")
-
-
-    def _parse_width(
-        self, image: str, value: str
-    ) -> ChangeWidth | None:
-        """Parse a width value.
-
-        Param image:
-            The reference to the image whos width will be changed.
-        
-        Param value:
-            The value of the new width.
-
-        Returns:
-            An event representing a request for changing the width of
-            of the image under the provided reference.
-
-        """
-        try:
-            width = int(value)
-
-        except ValueError:
-            return DisplayWarningRequested(f"Invalid width value: {value}")
-
-        if width <= 0:
-            return DisplayWarningRequested(f"Invalid width: {width}")
-
-        return ChangeWidth(image, width)
-
-    def _parse_height(
-        self, image: str, value: str
-    ) -> ChangeHeight | None:
-        """Parse a height value.
-
-        Param image:
-            A reference to the image whos height will be changed.
-
-        Param value:
-            The value of the new height.
-
-        Returns:
-            An event representing a request for changing the height of
-            the image under the provided reference.
-
-        """
-        try:
-            height = int(value)
-
-        except ValueError:
-            return DisplayWarningRequested(f"Invalid height value: {value}")
-
-        if height <= 0:
-            return DisplayWarningRequested(f"Invalid height: {height}")
-
-        return ChangeHeight(image, height)
-
-    def _parse_brightness(
-        self, image: str, value: str
-    ) -> ChangeBrightness | None:
-        """Parse a brightness value.
-
-        Param image:
-            A reference to the image who brightness will change.
-
-        Param value:
-            The value of the new brightness.
-
-        Returns:
-            An event representing a request for changing the brightne-
-            ss of the image under the provided reference.
-
-        """
-        try:
-            brightness = float(value)
-
-        except ValueError:
-            return DisplayWarningRequested(f"Invalid brightness value: {value}")
-
-        if brightness < 0:
-            return DisplayWarningRequested(f"Invalid brightness: {brightness}")
-
-        return ChangeBrightness(image, brightness)
-
-    def _parse_contrast(
-        self, image: str, value: str
-    ) -> ChangeContrast | None:
-        """Parse a contrast value.
-
-        Param image:
-            A reference to the image whos contrast will change.
-
-        Param value:
-            The value of the new contrast.
-
-        Returns:
-            An event representing a request for changing the contrast
-            of the image under the provided reference.
-
-        """
-        try:
-            contrast = float(value)
-        except ValueError:
-            return DisplayWarningRequested(f"Invalid contrast value: {value}")
-
-        if contrast < 0:
-            return DisplayWarningRequested(f"Invalid contrast: {contrast}")
-
-        return ChangeContrast(image, contrast)
-
-    def _parse_set_command(
-        self, command: str
-    ) -> ControllerEvent | None:
-        """Parse a set command.
-
-        Param command:
-            The 'set' command which will be parsed.
-
-        Returns:
-            A controller event related to the 'set' command, or a
-            'None' if any of the methods delegates fail.
-
-        """
-        parts = command.split()
-
-        if len(parts) != 3:
-            return DisplayWarningRequested(f"Invalid command: {command}")
-
-        image = parts[0]
-        property_name = parts[1].lower()
-        value = parts[2]
-
-        if property_name == "width":
-            return self._parse_width(image, value)
-
-        if property_name == "height":
-            return self._parse_height(image, value)
-
-        if property_name == "brightness":
-            return self._parse_brightness(image, value)
-
-        if property_name == "contrast":
-            return self._parse_contrast(image, value)
-
-        return DisplayWarningRequested(f"Invalid command: {command}")
-
-    def _parse_render_command(
-        self, command: str
-    ) -> RenderRequested | None:
-        """Parse a render command.
-
-        Param command:
-            A 'render' command which will be parsed.
-
-        Returns:
-            A controller event representing a request to render an im-
-            age, or 'None' if it fails to parse the 'set' command.
-
-        """
-        parts = command.split()
-
-        if not parts:
-            return RenderRequested()
-
-        if len(parts) == 1:
-            return RenderRequested(
-                image=parts[0]
-            )
-
-        if (
-            len(parts) == 3
-            and parts[1].lower() == "to"
-        ):
-            return RenderRequested(
-                image=parts[0], destination=parts[2]
-            )
-
-        return DisplayWarningRequested(f"Invalid render command: {command}")
-
-    def _parse_save_command(
-        self, command: str
-    ) -> SaveSessionRequested | None:
-        """Parse a save command.
-
-        Param command:
-            A 'save session as' command which will be parsed.
-
-        Returns:
-            A controller event representing a request to save a
-            session, or a none if the command is malformed.
-
-        """
-
-        parts = command.split()
-
-        if (len(parts) == 3
-            and parts[0].lower() == "session"
-            and parts[1].lower() == "as"
-        ):
-            filename = parts[2]
-
-            return SaveSessionRequested(filename)
-
-        return DisplayWarningRequested(f"Invalid save command: {command}")
-
-    def _parse_info_command(
-        self, command: str
-    ) -> ImageInfoRequested:
-        """Parse an info command.
-
-        Param command:
-            An info request command that will be parsed.
-
-        Returns:
-            A controllerevent representing a request to display infor-
-            mation of an image.
-
-        """
-        return ImageInfoRequested()
-
-    def _parse_quit_command(
-        self, command: str
-    ) -> ExitRequested:
-        """Parse a quit command.
-
-        Param command:
-            An exit command which will be parsed.           
-        Returns:
-            A controllerevent representing a request to exit the appl-
-            ication.
-            
-        """
-        return ExitRequested()
+        self._notify_observers(
+            DisplayNoticeRequested("AAS: ")
+        )
+        return input().strip()
 
     def start(self) -> None:
         """Start accepting commands.
